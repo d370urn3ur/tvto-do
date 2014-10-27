c4925ac14b1937b488088016ed996ea97fc2ca4f
Joshua Pierce <joshua.pierce@loyaltytechnology.com>
2014-10-27 17:45:38 +0100

    added activity graph dagger

--- app/src/main/java/the/autarch/tvto_do/activity/BaseEventActivity.java
+++ app/src/main/java/the/autarch/tvto_do/activity/BaseEventActivity.java
@@ -1,13 +1,40 @@
 package the.autarch.tvto_do.activity;
 
+import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
+import java.util.Arrays;
+import java.util.List;
+
+import dagger.ObjectGraph;
 import de.greenrobot.event.EventBus;
+import the.autarch.tvto_do.TVTDApplication;
+import the.autarch.tvto_do.dagger.ActivityModule;
 
 /**
  * Created by jpierce on 9/10/14.
  */
 public class BaseEventActivity extends ActionBarActivity {
+
+    private ObjectGraph _activityGraph;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TVTDApplication app = (TVTDApplication)getApplication();
+        _activityGraph = app.getApplicationGraph().plus(getModules().toArray());
+
+        _activityGraph.inject(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+
+        _activityGraph = null;
+
+        super.onDestroy();
+    }
 
     @Override
     protected void onStart() {
@@ -19,5 +46,18 @@ public class BaseEventActivity extends ActionBarActivity {
     protected void onStop() {
         super.onStop();
         EventBus.getDefault().unregister(this);
+    }
+
+    /**
+     * A list of modules to use for the individual activity graph. Subclasses can override this
+     * method to provide additional modules provided they call and include the modules returned by
+     * calling {@code super.getModules()}.
+     */
+    protected List<Object> getModules() {
+        return Arrays.<Object>asList(new ActivityModule(this));
+    }
+
+    public void inject(Object object) {
+        _activityGraph.inject(object);
     }
 }
---
+++ app/src/main/java/the/autarch/tvto_do/dagger/ActivityModule.java
@@ -0,0 +1,45 @@
+package the.autarch.tvto_do.dagger;
+
+import android.content.Context;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+import the.autarch.tvto_do.activity.BaseEventActivity;
+import the.autarch.tvto_do.activity.ShowsListActivity;
+import the.autarch.tvto_do.fragment.ShowsListFragment;
+import the.autarch.tvto_do.fragment.ShowsSearchFragment;
+
+/**
+ * Created by joshua.pierce on 27/10/14.
+ */
+@Module(
+        injects = {
+                ShowsListActivity.class,
+                ShowsListFragment.class,
+                ShowsSearchFragment.class
+        },
+        addsTo = ApplicationModule.class,
+        library = true
+)
+public class ActivityModule {
+
+    private final BaseEventActivity activity;
+
+    public ActivityModule(BaseEventActivity activity) {
+        this.activity = activity;
+    }
+
+    /**
+     * Allow the activity context to be injected but require that it be annotated with
+     * {@link ForActivity @ForActivity} to explicitly differentiate it from application context.
+     */
+    @Provides
+    @Singleton
+    @ForActivity
+    Context provideActivityContext() {
+        return activity;
+    }
+
+}
